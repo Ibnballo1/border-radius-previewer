af6473ddfbaa01d84c2ed5e9bcad59085d745709
Ibnballo1 <belloabdullateef035@gmail.com>
2023-01-10 16:41:29 +0100

    Renove unused and commented code

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 const allRadius = document.getElementById("borderradius");
-const allInput = document.querySelectorAll(".input");
 const box = document.getElementById("box");
 const reset = document.getElementById("reset");
 const copyText = document.getElementById("copy");
-let length = 0;
-let split = '';
-let borderRadiusValue = 0;
-// const str = 'this is a boy'
-// console.log(str.slice(0, 4));
-// console.log(str.slice(1, (str.indexOf(' '))));
-// console.log(`all radius value is ${allRadius.value}`);
+
 allRadius.addEventListener('input', (e) => {
+  let split = '';
+  let borderRadiusValue = 0;
   borderRadiusValue = e.target.value;
   split = borderRadiusValue.split(" ");
-  // console.log(split);
+
   if (split.length == 4) {
     box.style.borderTopLeftRadius = split[0];
     box.style.borderTopRightRadius = split[1];
@@ -43,7 +38,6 @@ reset.addEventListener("click", () => {
 copyText.addEventListener("click", () => {
   allRadius.select();
   allRadius.setSelectionRange(0, 99999); // For Mobile Devices
-  // Copy the text inside the text field
   navigator.clipboard.writeText(allRadius.value);
   console.log(allRadius.value);
   alert("Border Radius: " + allRadius.value);
